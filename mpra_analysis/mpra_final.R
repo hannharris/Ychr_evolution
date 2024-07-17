f240dd9f38ddc598bf15d54925a1c60321ee837a
Hannah Harris <hannharris7@gmail.com>
2024-07-17 12:16:51 -0400

    Update mpra_final.R

--- mpra_analysis/mpra_final.R
+++ mpra_analysis/mpra_final.R
@@ -3,9 +3,6 @@
 
 #load packages
 
-#install.packages("DescTools")
-#library(DescTools)
-
 library(DescTools) 
 library(ggplot2)
 library(ggbreak)
@@ -21,20 +18,18 @@ scalescolors <- c("#66C2A5", "#FC8D62", "#8DA0CB")
 xy_colors <- c('#1B9E77','#7570B3') 
 expr_vs_prom_colors <- c('#A6CEE3','#1F78B4') 
 
-
 #load data 
-
-start_sites <- read.delim("/lab/solexa_page/hannah/supp_info/tables/starts.txt")
+myPath <- #ADD PATH TO GITHUB FOLDER
+start_sites <- read.delim(paste0(myPath, "/tables/starts.txt"))
 start_sites <- rbind(start_sites, data.frame("gene" = c("EEF1A1", "ACTB", "CMV"), "start" = c(73521032, 5530709, 1))) #positive ctls 
 
-fib_counts_1 <- read.delim("/lab/solexa_page/hannah/220516_mpra/Count_Basic/work/32/cc2c02d5c079f4b217c5d91737a57e/Fib_7169_1_counts.tsv")
-fib_counts_2 <- read.delim("/lab/solexa_page/hannah/220516_mpra/Count_Basic/work/96/bc3fc6b4ba1178ac8933a8a9611c6b/Fib7276_2_counts.tsv") 
-lcl_counts <- read.delim("/lab/solexa_page/hannah/220516_mpra/LCL_1_counts.tsv")
+fib_counts_1 <- read.delim(paste0(myPath, "/tables/Fib_7169_1_counts.tsv"))
+fib_counts_2 <- read.delim(paste0(myPath, "/tables/Fib7276_2_counts.tsv"))
+lcl_counts <- read.delim(paste0(myPath, "/tables/LCL_1_counts.tsv"))
 lcl_counts <- lcl_counts[,c(1,5,6)]
 colnames(lcl_counts) <- c('name', 'lcl_log2', 'lcl_n_obs_bc')
 
-#gene_pairs <- read.delim("/lab/solexa_page/hannah/220516_mpra/g_p.txt")
-gene_pairs <- read.delim("/lab/solexa_page/hannah/220516_mpra/msa/long_alignments/g_p.txt") 
+gene_pairs <- read.delim(paste0(myPath, "/tables/g_p.txt")) 
 gene_pairs$gene <- gene_pairs$gene.x 
 gene_pairs$gene.x <- NULL
 gene_pairs <- gene_pairs %>%  filter(gene != "USP9Yb" & gene != "PRKX"
@@ -46,7 +41,7 @@ gene_pairs <- gene_pairs %>%  filter(gene != "USP9Yb" & gene != "PRKX"
                        & gene != "TXLNG"
                        & gene != "TXLNGY")
 
-metadata <- read.delim("/lab/solexa_page/hannah/220516_mpra/metadata_0119.txt")
+metadata <- read.delim(paste0(myPath, "/tables/metadata_0119.txt"))  
 metadata <- metadata %>% filter(gene != "USP9Yb" & gene != "PRKX"
                        & gene != "PRKY"
                        & gene != "NLGN4X"
@@ -67,24 +62,11 @@ full_fib <- full_fib[,c(1,3,4,5,7,6)]
 
 
 
-
-#make bed for ucsc genome browser
-
-beds <- make_bedgraph(full_fib, 'file_nameUCSC.txt', 'name')
-
-beds <- beds[[2]] %>% filter(chromosome != "cmv")
-beds <- separate(beds, c(3), c("A", "b"), sep = "_")
-beds <- beds %>% filter(b != "shuff")
-beds$b <- NULL
-write.table(x = beds, file = "/lab/solexa_page/hannah/supp_info/file_nameUCSC.txt", quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
-
-
-
 #fig. S10A library composition
 
 df_n_t <- data.frame('type' = c("negative_ctls", "positive_ctls", 'test_seqs'), 'values' = c(240, 103, 1232))
 
-pdf(file=("/lab/solexa_page/hannah/supp_info/figures/S10_lib_content.pdf"), width=4, height=4, colormodel = "rgb") 
+pdf(file=paste0(myPath, "/S10_lib_content.pdf"))  
 
 ggplot(df_n_t, aes(x=type, y=values)) + 
   geom_col(width = 0.2) + 
@@ -97,7 +79,7 @@ dev.off()
 
 #correlation between libs 
 
-pdf(file=("/lab/solexa_page/hannah/supp_info/figures/S10_corr_libs.pdf"), width=4, height=4, colormodel = "rgb") 
+pdf(file=paste0(myPath, "/S10_corr_libs.pdf"), width=4, height=4, colormodel = "rgb") 
 ggplot(m_td, aes(x= fib_1_log2, y= fib_2_log2)) + 
   stat_cor(method = "pearson") + 
   geom_smooth(method='lm', formula= y~x, se= FALSE) + 
@@ -108,7 +90,7 @@ ggplot(m_td, aes(x= fib_1_log2, y= fib_2_log2)) +
   stat_regline_equation(label.y = 5) 
 dev.off()
 
-pdf(file=("/lab/solexa_page/hannah/supp_info/figures/4C_corr_full_fib_lcl.pdf"), width=4, height=4, colormodel = "rgb")
+pdf(file=paste0(myPath, "/4C_corr_full_fib_lcl.pdf"), width=4, height=4, colormodel = "rgb")
 ggplot(m_td %>% filter(type != "positive ctl" & type != "negative ctl" & gene != "USP9Yb" & gene != "PRKX"
                        & gene != "PRKY"
                        & gene != "NLGN4X"
@@ -127,19 +109,19 @@ ggplot(m_td %>% filter(type != "positive ctl" & type != "negative ctl" & gene !=
 dev.off()
 
 #histogram of n_bcs 
-pdf(file=("/lab/solexa_page/hannah/supp_info/figures/S10_rep1_fib_bcs.pdf"), width=4, height=4, colormodel = "rgb") 
+pdf(file= paste0(myPath,"S10_rep1_fib_bcs.pdf"), width=4, height=4, colormodel = "rgb") 
 ggplot(m_td, aes(x=fib_1_n_obs_bc)) +
   geom_histogram() +
   stat_bin(bins = 50)+
   theme_pubclean()
 dev.off()
-pdf(file=("/lab/solexa_page/hannah/supp_info/figures/S10_fib_bcs.pdf"), width=4, height=4, colormodel = "rgb") 
+pdf(file=paste0(myPath,"S10_fib_bcs.pdf"), width=4, height=4, colormodel = "rgb") 
 ggplot(m_td, aes(x=fib_2_n_obs_bc)) +
 geom_histogram() +
   stat_bin(bins = 50) +
   theme_pubclean()
 dev.off()
-pdf(file=("/lab/solexa_page/hannah/supp_info/figures/S10_lcl_bcs.pdf"), width=4, height=4, colormodel = "rgb") 
+pdf(file=paste0(myPath,"S10_lcl_bcs.pdf"), width=4, height=4, colormodel = "rgb") 
 ggplot(m_td %>% filter(name!= 'no_BC'), aes(x=lcl_n_obs_bc)) +
 geom_histogram() +
   stat_bin(bins = 50) +
@@ -177,8 +159,7 @@ full_fib_200 <- read.delim("/lab/solexa_page/hannah/supp_info/tables/full_fib_20
 
 
 
-#get distances 
-  for control plots and for graphing controls based on distance to TSS
+#get distances for control plots and for graphing controls based on distance to TSS
 
 
 lcl_50_dist <- get_dist_TSS(lcl_50, start_sites)
@@ -188,9 +169,9 @@ full_fib_200_dist <- get_dist_TSS(full_fib_200, start_sites)
 
 make_bedgraph(full_fib_50_dist, file_name, column_to_sep)
 
-#graph controls
+#graph controls HERE WITH THE EDITING
 
-pdf(file=("/lab/solexa_page/hannah/supp_info/figures/4B_fib_ctls.pdf"), width=4, height=4, colormodel = "rgb")
+pdf(file= ("/lab/solexa_page/hannah/supp_info/figures/4B_fib_ctls.pdf"), width=4, height=4, colormodel = "rgb")
 
 full_fib_dist_200_1 <- full_fib_200_dist %>% filter(distance < 500 & distance > -500)
 res.kruskal <- full_fib_dist_200_1 %>% kruskal_test(log2 ~ type)
